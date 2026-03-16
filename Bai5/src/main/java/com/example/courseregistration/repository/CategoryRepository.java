6634f73e89c0ed7906b0b911eaaae9cbeeb84bf7
Dang Van Hau <dangvanhau0404@gmail.com>
2026-03-16 16:11:13 +0700

    Delete Bai5 directory

--- Bai5/src/main/java/com/example/courseregistration/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.courseregistration.repository;
-
-import com.example.courseregistration.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
-
